c62a342409866ae614e29e61ff9cc0122baeec1c
David Barrientos <david.barrientos.f@gmail.com>
2017-02-21 19:47:57 -0300

    add column discount to category model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
 	validates :price, presence: true
 
 	before_save :premium_default
+	after_destroy :last_product
 
 	scope :premium_all, -> { where(premium: true)}
 	scope :last_n, ->(n) { limit(n) }
@@ -12,5 +13,18 @@ class Product < ApplicationRecord
 		unless self.premium.present? || self.premium == true
 			self.premium = false
 		end
+	end
+
+	def last_product
+		categorias = Category.all
+		categorias.each do |categoria|
+			if categoria.products.empty?
+				categoria.destroy
+			end
+		end
+	end
+
+	def precio_final
+		@precio_final = self.price - ((self.price * self.category.discount).to_f / 100)
 	end
 end
