556b3f2ba2cfdd582603c8e6e9e870af0b13e5c9
David Barrientos <david.barrientos.f@gmail.com>
2017-02-21 18:58:29 -0300

    add price and premium to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+	belongs_to :category
+	validates :price, presence: true
+
+	before_save :premium_default
+	# validates :category_id, presence: true #esto es para rails 4
+
+	def premium_default
+		unless self.premium.present? || self.premium == true
+			self.premium = false
+		end
+	end
 end
