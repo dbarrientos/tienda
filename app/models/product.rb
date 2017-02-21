833e4cf4eef7e174f8e6a0534162fb9af2b0daef
David Barrientos <david.barrientos.f@gmail.com>
2017-02-21 19:24:45 -0300

    add scope premium_all and last_n into product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
 	validates :price, presence: true
 
 	before_save :premium_default
+
+	scope :premium_all, -> { where(premium: true)}
+	scope :last_n, ->(n) { limit(n) }
 	# validates :category_id, presence: true #esto es para rails 4
 
 	def premium_default
